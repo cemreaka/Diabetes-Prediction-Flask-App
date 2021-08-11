19347244e06c5e7bfdf790821d4d48f27df36f6b
cemreaka <cemreaka@gmail.com>
2021-08-11 13:57:17 +0300

    modified

--- model/model.py
+++ model/model.py
@@ -8,12 +8,12 @@ DiabetesPrediction = p.read_csv(
 
 X = DiabetesPrediction[['Pregnancies', 'Glucose', 'BloodPressure',
                         'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]
-Y = DiabetesPrediction['Outcome']
+y = DiabetesPrediction['Outcome']
 
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.3, random_state=10)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=10)
 
 lm = LogisticRegression(solver='liblinear')
-lm.fit(X_train, Y_train)
+lm.fit(X_train, y_train)
 
 pickle.dump(lm, open('model.pickle', 'wb'))
